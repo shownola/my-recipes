083de458cb92b8c5447196d7112dffb4241b3a60
SHERRY <swasieko@gmail.com>
2017-03-27 19:19:30 +0000

    Add one to many associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170325185049) do
+ActiveRecord::Schema.define(version: 20170327183445) do
 
   create_table "chefs", force: :cascade do |t|
     t.string   "chefname"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20170325185049) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "chef_id"
   end
 
 end
--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -3,7 +3,13 @@ require 'test_helper'
 class RecipeTest < ActiveSupport::TestCase
   
   def setup
-    @recipe = Recipe.new(name: "Vegetable Stir Fry", description: "Great asian cuisine")
+    @chef = Chef.create!(chefname: "Jackson", email: "jackson@email.com")
+    @recipe = @chef.recipes.build(name: "Vegetable Stir Fry", description: "Great asian cuisine")
+  end
+  
+  test "recipe without a chef should be invalid" do
+    @recipe.chef_id = nil
+    assert_not @recipe.valid?
   end
   
   test "recipe should be valid" do
